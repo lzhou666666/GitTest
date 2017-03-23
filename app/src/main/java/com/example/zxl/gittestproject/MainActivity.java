4951330b7d9f6f8ba8a803e692f10e9e09d5a496
lzhou666666 <15015640793@163.com>
2017-03-23 17:36:41 +0800

    add new file and fourth modified

--- app/src/main/java/com/example/zxl/gittestproject/MainActivity.java
+++ app/src/main/java/com/example/zxl/gittestproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.zxl.gittestproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTvMyTest;
@@ -16,5 +18,9 @@ public class MainActivity extends AppCompatActivity {
         mTvMyTest.setText("第一次修改并提交到github等提交的测试");
 
         mTvMyTest.setTextColor(0xff00ff00);
+    }
+    
+    public void btnTestClick(View btnView) {
+        Toast.makeText(this, "btn的点击事件", Toast.LENGTH_SHORT).show();
     }
 }
