36df5d0b0a0e539d59832afadc70314bec66f2df
lzhou666666 <15015640793@163.com>
2017-03-23 17:03:40 +0800

    secend modified and commit to github

--- app/src/main/java/com/example/zxl/gittestproject/MainActivity.java
+++ app/src/main/java/com/example/zxl/gittestproject/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.zxl.gittestproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTvMyTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mTvMyTest = (TextView) this.findViewById(R.id.tv_my_test);
+        mTvMyTest.setText("第一次修改并提交到github等提交的测试");
     }
 }
